038236ad4959cff08f1835f368c6d1d60bfc8c7c
Stormy <stormy@mailfence.com>
2024-10-06 07:11:04 +0200

    curses: curse spell has different runes to weaken/confuse
    reported by jan, thanks

--- data/lua/rs1/skill_magic/curses.lua
+++ data/lua/rs1/skill_magic/curses.lua
@@ -2,12 +2,22 @@
 -- https://classic.runescape.wiki/w/Weaken
 -- https://classic.runescape.wiki/w/Curse
 
-function remove_curse_runes(player)
+local function remove_confuse_runes(player)
 	if not worn(player, "staff of water") then
 		remove(player, "water-rune", 3)
 	end
 	if not worn(player, "staff of earth") then
 		remove(player, "earth-rune", 2)
+	end
+	remove(player, "body-rune", 1)
+end
+
+local function remove_curse_runes(player)
+	if not worn(player, "staff of water") then
+		remove(player, "water-rune", 2)
+	end
+	if not worn(player, "staff of earth") then
+		remove(player, "earth-rune", 3)
 	end
 	remove(player, "body-rune", 1)
 end
@@ -18,7 +28,7 @@ function skillplayer_confuse(player, target)
 	else
 		mes(player, "@que@Cast spell successfully")
 		advancestat(player, STAT_MAGIC, 104, 0)
-		remove_curse_runes(player)
+		remove_confuse_runes(player)
 		shootplayer(player, target, "dummybolt")
 		substat(target, STAT_ATTACK, 1, 5)
 		mes(target, "Your attack has been reduced by a confuse spell!")
@@ -31,7 +41,7 @@ register_skillnpc("_", "confuse", function(player, target)
 	else
 		mes(player, "@que@Cast spell successfully")
 		advancestat(player, STAT_MAGIC, 104, 0)
-		remove_curse_runes(player)
+		remove_confuse_runes(player)
 		shootnpc(player, target, "dummybolt")
 		npcsubstat(target, STAT_ATTACK, 1, 5)
 	end
@@ -43,7 +53,7 @@ function skillplayer_weaken(player, target)
 	else
 		mes(player, "@que@Cast spell successfully")
 		advancestat(player, STAT_MAGIC, 168, 0)
-		remove_curse_runes(player)
+		remove_confuse_runes(player)
 		shootplayer(player, target, "dummybolt")
 		substat(target, STAT_STRENGTH, 1, 5)
 		mes(target, "Your strength has been reduced by a weaken spell!")
@@ -56,7 +66,7 @@ register_skillnpc("_", "weaken", function(player, target)
 	else
 		mes(player, "@que@Cast spell successfully")
 		advancestat(player, STAT_MAGIC, 168, 0)
-		remove_curse_runes(player)
+		remove_confuse_runes(player)
 		shootnpc(player, target, "dummybolt")
 		npcsubstat(target, STAT_STRENGTH, 1, 5)
 	end
