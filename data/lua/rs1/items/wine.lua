30a98caa2b1a961c72d4869cbbd081ba76a56cc6
Stormy <stormy@mailfence.com>
2024-12-14 11:02:00 +0100

    wine: implement half full wine jug

--- data/lua/rs1/items/wine.lua
+++ data/lua/rs1/items/wine.lua
@@ -1,6 +1,5 @@
--- ./rscplus-replays-master/RSC 2001/replays master archive/Stat effects/Consumable Stat effects/Alcohol/jug of wine- (level 1-1-1 base)
-
 function opinv_wine(player)
+	-- ./rscplus-replays-master/RSC 2001/replays master archive/Stat effects/Consumable Stat effects/Alcohol/jug of wine- (level 1-1-1 base)
 	thinkbubble(player, "wine")
 	mes(player, "@que@You drink the wine")
 	mes(player, "@que@It makes you feel a bit dizzy")
@@ -10,6 +9,21 @@ function opinv_wine(player)
 	if statdown(player, STAT_HITS) then
 		-- Logg/Tylerbeg/07-26-2018 11.44.01 mapping the kharazi jungle.pcap
 		healstat(player, STAT_HITS, 11, 0)
+		mes(player, "@que@But it heals some health")
+	end
+end
+
+function opinv_half_full_wine_jug(player)
+	-- https://www.youtube.com/watch?v=3Zv4G_t3TWI&t=658
+	thinkbubble(player, "wine")
+	mes(player, "@que@You drink the wine")
+	mes(player, "@que@It makes you feel a bit dizzy")
+	remove(player, "half full wine jug", 1)
+	give(player, "jug", 1)
+	-- XXX: not confirmed, taken from OSRS
+	substat(player, STAT_ATTACK, 2, 0)
+	if statdown(player, STAT_HITS) then
+		healstat(player, STAT_HITS, 7, 0)
 		mes(player, "@que@But it heals some health")
 	end
 end
