e27e57d8e1323c4df46c52bf0995b0d34c1c2490
Stormy <stormy@mailfence.com>
2024-12-14 10:55:20 +0100

    incomplete disk of returning functionality

---
+++ data/lua/rs1/items/asgarnia/disk_of_returning.lua
@@ -0,0 +1,5 @@
+-- ./Logg/Tylerbeg/08-05-2018 13.14.13 @@ disk of returning science.pcap
+
+function opinv_disk_of_returning(player)
+	mes(player, "The disk will only work from in Thordur's black hole")
+end
