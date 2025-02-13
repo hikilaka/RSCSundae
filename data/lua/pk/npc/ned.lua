b41dd92cbb736d49a1714c333ba2dba7704751b0
Stormy <stormy@mailfence.com>
2025-02-14 00:04:54 +0100

    pk: better script for ned

--- data/lua/pk/npc/ned.lua
+++ data/lua/pk/npc/ned.lua
@@ -6,6 +6,12 @@ function talknpc_ned(player, npc)
 	if resp == 1 then
 		say(player, "Yes please")
 		give(player, "lobster", 30)
+		if male(player) then
+			npcsay(npc, "Here you go lad")
+		else
+			npcsay(npc, "Here you go lass")
+		end
+		say(player, "Thanks Ned")
 	elseif resp == 2 then
 		say(player, "No thanks")
 	end
