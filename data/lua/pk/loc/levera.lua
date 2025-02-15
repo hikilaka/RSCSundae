6141518f6f4a649071c649b183f47273a0858a15
Stormy <stormy@mailfence.com>
2025-02-15 22:28:56 +0100

    levera: add showeffect calls

--- data/lua/pk/loc/levera.lua
+++ data/lua/pk/loc/levera.lua
@@ -8,11 +8,15 @@ function oploc1_levera(player, x, y)
 		"I'll stay where I am")
 	if resp == 1 then
 		teleport(player, 116, 364)
+		showeffect(0, 166, 363)
 	elseif resp == 2 then
 		teleport(player, 160, 312)
+		showeffect(0, 160, 311)
 	elseif resp == 3 then
 		teleport(player, 237, 247)
+		showeffect(0, 237, 246)
 	elseif resp == 4 then
 		teleport(player, 320, 163)
+		showeffect(0, 320, 162)
 	end
 end
