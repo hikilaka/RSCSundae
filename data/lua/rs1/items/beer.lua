bc53f2e4e4b49f7f7bc7e76b70de40d42f3308f7
Stormy <stormy@mailfence.com>
2025-07-02 15:40:19 +0200

    beer: drinking should give a beer glass in 2002

--- data/lua/rs1/items/beer.lua
+++ data/lua/rs1/items/beer.lua
@@ -3,6 +3,7 @@
 function opinv_beer(player)
 	thinkbubble(player, "beer")
 	remove(player, "beer", 1)
+	give(player, "beer glass", 1)
 	mes(player, "@que@You drink the beer")
 	mes(player, "@que@You feel slightly reinvigorated")
 	healstat(player, STAT_HITS, 1, 0)
