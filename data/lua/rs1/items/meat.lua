e228bedf7ffe8acf5c59bf05cd24ff98bb9b0ffa
Stormy <stormy@mailfence.com>
2025-07-04 11:27:30 +0200

    food: minor (non-functional) change to packet ordering

--- data/lua/rs1/items/meat.lua
+++ data/lua/rs1/items/meat.lua
@@ -1,8 +1,8 @@
 -- ./rscplus-replays-master/rsc-preservation.xyz/Tutorial Island/Tutorial Island 03 06 2018
 
 function opinv_cookedmeat(player)
-	mes(player, "@que@You eat the meat")
 	remove(player, "cookedmeat", 1)
+	mes(player, "@que@You eat the meat")
 	if statdown(player, STAT_HITS) then
 		healstat(player, STAT_HITS, 3, 0)
 		mes(player, "@que@It heals some health")
--- data/lua/rs1/items/wine.lua
+++ data/lua/rs1/items/wine.lua
@@ -1,10 +1,10 @@
 function opinv_wine(player)
 	-- ./rscplus-replays-master/RSC 2001/replays master archive/Stat effects/Consumable Stat effects/Alcohol/jug of wine- (level 1-1-1 base)
 	thinkbubble(player, "wine")
-	mes(player, "@que@You drink the wine")
-	mes(player, "@que@It makes you feel a bit dizzy")
 	remove(player, "wine", 1)
 	give(player, "jug", 1)
+	mes(player, "@que@You drink the wine")
+	mes(player, "@que@It makes you feel a bit dizzy")
 	substat(player, STAT_ATTACK, 3, 0)
 	if statdown(player, STAT_HITS) then
 		-- Logg/Tylerbeg/07-26-2018 11.44.01 mapping the kharazi jungle.pcap
@@ -16,10 +16,10 @@ end
 function opinv_half_full_wine_jug(player)
 	-- https://www.youtube.com/watch?v=3Zv4G_t3TWI&t=658
 	thinkbubble(player, "wine")
-	mes(player, "@que@You drink the wine")
-	mes(player, "@que@It makes you feel a bit dizzy")
 	remove(player, "half full wine jug", 1)
 	give(player, "jug", 1)
+	mes(player, "@que@You drink the wine")
+	mes(player, "@que@It makes you feel a bit dizzy")
 	-- XXX: not confirmed, taken from OSRS
 	substat(player, STAT_ATTACK, 2, 0)
 	if statdown(player, STAT_HITS) then
