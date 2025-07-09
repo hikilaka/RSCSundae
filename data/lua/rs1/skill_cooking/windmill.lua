a62c7603310c1a5fed7deaf33a8565588db8eba6
Stormy <stormy@mailfence.com>
2025-07-09 18:25:40 +0200

    windmill: refactor, make it easier to add more windmills

--- data/lua/rs1/skill_cooking/windmill.lua
+++ data/lua/rs1/skill_cooking/windmill.lua
@@ -1,4 +1,29 @@
 -- rsc-preservation.xyz/Quests/cooks-assistant.pcap
+-- ./Logg/Tylerbeg/08-01-2018 19.32.34 varrock pt2.pcap
+
+local function use_hopper(player, varname)
+	local hopper_grain = getvar(player, varname)
+	if hopper_grain > 0 then
+		mes(player, "@que@There is already grain in the hopper")
+	else
+		thinkbubble(player, "grain")
+		remove(player, "grain", 1)
+		mes(player, "@que@You put the grain in the hopper")
+		setvar(player, varname, 1)
+	end
+end
+
+local function operate_hopper(player, varname, x, y)
+	local hopper_grain = getvar(player, varname)
+	mes(player, "@que@You operate the hopper")
+	if hopper_grain > 0 then
+		mes(player, "@que@The grain slides down the chute")
+		addobject(player, "flourheap", 1, x, y)
+		setvar(player, varname, 0)
+	else
+		mes(player, "@que@Nothing interesting happens")
+	end
+end
 
 function takeobj_flourheap(player, x, y)
 	-- RSC 2001/replays master archive/Item info/Alphabetical/P/itemspawn-pot of flour (chute)- attempt to pick up without pot.pcap
@@ -6,37 +31,25 @@ function takeobj_flourheap(player, x, y)
 	mes(player, "@que@I need a pot to hold it in")
 end
 
-function useobj_flourheap_pot(player, x, y)
+register_useobj("flourheap", "pot", function(player, x, y)
 	delobject(player, "flourheap", x, y)
 	mes(player, "@que@You put the flour in the pot")
 	remove(player, "pot", 1)
 	give(player, "flourpot", 1)
-end
+end)
 
-register_useobj("flourheap", "pot", useobj_flourheap_pot)
-
-function useloc_hopper1_grain(player, x, y)
-	local hopper_grain = getvar(player, "hopper_grain")
-	if hopper_grain > 0 then
-		mes(player, "@que@There is already grain in the hopper")
-	else
-		thinkbubble(player, "grain")
-		remove(player, "grain", 1)
-		mes(player, "@que@You put the grain in the hopper")
-		setvar(player, "hopper_grain", 1)
-	end
-end
-
-register_useloc("hopper1", "grain", useloc_hopper1_grain)
+register_useloc("hopper1", "grain", function(player, x, y)
+	use_hopper(player, "hopper_grain")
+end)
 
 function oploc1_hopper1(player)
-	local hopper_grain = getvar(player, "hopper_grain")
-	mes(player, "@que@You operate the hopper")
-	if hopper_grain > 0 then
-		mes(player, "@que@The grain slides down the chute")
-		addobject(player, "flourheap", 1, 166, 599)
-		setvar(player, "hopper_grain", 0)
-	else
-		mes(player, "@que@Nothing interesting happens")
-	end
+	operate_hopper(player, "hopper_grain", 166, 599)
+end
+
+register_useloc("hopper2", "grain", function(player, x, y)
+	use_hopper(player, "hopper2_grain")
+end)
+
+function oploc1_hopper2(player)
+	operate_hopper(player, "hopper2_grain", 179, 481)
 end
